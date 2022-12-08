4a15c4d87c0e50be0861d1440e6a0f1403e49102
Mohammed Badawy <115544356+Mohammed-Badawy@users.noreply.github.com>
2022-12-08 22:03:31 +0200

    Add some references for DOM elements

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Add some DOM elements references
+const screen = document.querySelector(".current-value");
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector("#clear-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const signBtn = document.querySelector("#sign-btn");
+const percentBtn = document.querySelector("#percentage");
