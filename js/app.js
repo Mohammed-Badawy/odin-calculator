25d817b14396b5701c0e5193aece38a409fab585
Mohammed Badawy <115544356+Mohammed-Badawy@users.noreply.github.com>
2022-12-08 22:12:37 +0200

    Add some maths functions
    Added maths functions for addition, subtraction,
    multiplication and division. Also added operate
    function to do specific maths operation on operands.

--- js/app.js
+++ js/app.js
@@ -6,3 +6,70 @@ const clearBtn = document.querySelector("#clear-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const signBtn = document.querySelector("#sign-btn");
 const percentBtn = document.querySelector("#percentage");
+
+
+
+
+
+
+
+// add operate function
+function operate(num1, num2, oper)
+{
+    let sum = 0;
+
+    if(oper === "+")
+    {
+        sum = add(num1, num2);
+    }
+    else if(oper === "-")
+    {
+        sum = subtract(num1, num2);
+    }
+    else if(oper === "×")
+    {
+        sum = multiply(num1, num2);
+    }
+    else if(oper === "÷")
+    {
+        sum = divide(num1, num2);
+        if(sum === "Error")
+        {
+            return sum;
+        }
+        else
+        {
+            sum = Math.round(sum * 100) / 100; // round result to 2 decimals
+        }
+    }
+
+    return sum;
+}
+
+// add some basic math functions
+function add(n1, n2)
+{
+    return n1 + n2;
+}
+
+function subtract(n1, n2)
+{
+    return n1 - n2;
+}
+
+function multiply(n1, n2)
+{
+    return n1 * n2;
+}
+
+function divide(n1, n2)
+{
+    if(n2 === 0)
+    {
+        return "Error";
+    }
+    else
+    {
+        return n1 / n2;
+    }
+}
